26cfd718730f5891f92bec36d1a4f7a6b4476016
Chris Hicks <khandrish@gmail.com>
2016-10-29 16:22:22 -0700

    remove unused callbacks

--- lib/execs/initializer.ex
+++ lib/execs/initializer.ex
@@ -13,12 +13,4 @@ defmodule Execs.Initializer do
     :ok = @client.initialize()
     {:ok, %{}}
   end
-
-  def handle_call(_msg, _from, state) do
-    {:reply, :ok, state}
-  end
-
-  def handle_cast(_msg, state) do
-    {:noreply, state}
-  end
 end

d6fbd8f1cde964b0ab1893dee1da69e2e6531c6c
Chris Hicks <khandrish@gmail.com>
2016-10-30 23:08:20 -0700

    Fix spacing

--- lib/execs.ex
+++ lib/execs.ex
@@ -1,14 +1,14 @@
 defmodule Execs do
   use Application
 
-    def start(_type, _args) do
-      import Supervisor.Spec, warn: false
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
 
-      children = [
-        worker(Execs.Initializer, [])
-      ]
+    children = [
+      worker(Execs.Initializer, [])
+    ]
 
-      opts = [strategy: :one_for_one, name: Execs.Supervisor]
-      Supervisor.start_link(children, opts)
-    end
+    opts = [strategy: :one_for_one, name: Execs.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
 end

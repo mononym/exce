ecb974af6256a357ee796f7af52b8c5b0f780916
Chris Hicks <khandrish@gmail.com>
2016-10-30 23:54:28 -0700

    Add API to application module, along with tests

--- lib/execs.ex
+++ lib/execs.ex
@@ -1,6 +1,14 @@
 defmodule Execs do
   use Application
 
+  @client Application.get_env(:execs, :client_module)
+
+
+  #
+  # Application callback
+  #
+
+
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
@@ -11,4 +19,179 @@ defmodule Execs do
     opts = [strategy: :one_for_one, name: Execs.Supervisor]
     Supervisor.start_link(children, opts)
   end
+
+
+  #
+  # Application API
+  #
+
+
+  def initialize, do: @client.initialize()
+
+  def transaction(block), do: @client.transaction(block)
+
+  def create, do: hd(create(1))
+  def create(n), do: @client.create(n)
+
+  # delete
+
+  def delete(ids) when is_list(ids), do: @client.delete(ids)
+  def delete(id), do: hd(delete([id]))
+
+  def delete(ids, components) when is_list(ids) do
+    @client.delete(ids, enforce_list(components))
+  end
+
+  def delete(id, components) do
+    hd(delete([id], components))
+  end
+
+  def delete(ids, components, keys) when is_list(ids) do
+    @client.delete(ids, enforce_list(components), enforce_list(keys))
+  end
+
+  def delete(id, components, keys) do
+    hd(delete([id], components, keys))
+  end
+
+  # has_all
+
+  def has_all(ids, components) when is_list(ids) do
+    @client.has_all(ids, enforce_list(components))
+  end
+
+  def has_all(id, components), do: hd(has_all([id], components))
+
+  def has_all(ids, components, keys) when is_list(ids) do
+    @client.has_all(ids, enforce_list(components), enforce_list(keys))
+  end
+
+  def has_all(id, components, keys), do: hd(has_all([id], components, keys))
+
+  def has_all(ids, components, keys, functions) when is_list(ids) do
+    @client.has_all(ids, enforce_list(components), enforce_list(keys), enforce_list(functions))
+  end
+
+  def has_all(id, components, keys, functions) do
+    hd(has_all([id], components, keys, functions))
+  end
+
+  # has_any
+
+  def has_any(ids, components) when is_list(ids) do
+    @client.has_any(ids, enforce_list(components))
+  end
+
+  def has_any(id, components), do: hd(has_any([id], components))
+
+  def has_any(ids, components, keys) when is_list(ids) do
+    @client.has_any(ids, enforce_list(components), enforce_list(keys))
+  end
+
+  def has_any(id, components, keys), do: hd(has_any([id], components, keys))
+
+  def has_any(ids, components, keys, functions) when is_list(ids) do
+    @client.has_any(ids, enforce_list(components), enforce_list(keys), enforce_list(functions))
+  end
+
+  def has_any(id, components, keys, functions) do
+    hd(has_any([id], components, keys, functions))
+  end
+
+  # has_which
+
+  def has_which(ids, components) when is_list(ids) do
+    @client.has_which(ids, enforce_list(components))
+  end
+
+  def has_which(id, components), do: hd(has_which([id], components))
+
+  def has_which(ids, components, keys) when is_list(ids) do
+    @client.has_which(ids, enforce_list(components), enforce_list(keys))
+  end
+
+  def has_which(id, components, keys), do: hd(has_which([id], components, keys))
+
+  def has_which(ids, components, keys, functions) when is_list(ids) do
+    @client.has_which(ids, enforce_list(components), enforce_list(keys), enforce_list(functions))
+  end
+
+  def has_which(id, components, keys, functions) do
+    hd(has_which([id], components, keys, functions))
+  end
+
+  # list
+
+  def list(ids) when is_list(ids), do: @client.list(ids)
+  def list(id), do: hd(list([id]))
+
+  def list(ids, components) when is_list(ids) do
+    @client.list(ids, enforce_list(components))
+  end
+
+  def list(id, components), do: hd(list([id], components))
+
+  # find_with_all
+
+  def find_with_all(components) do
+    @client.find_with_all(components)
+  end
+
+  def find_with_all(components, keys) do
+    @client.find_with_all(components, keys)
+  end
+
+  def find_with_all(components, keys, functions) do
+    @client.find_with_all(components, keys, functions)
+  end
+
+  # find_with_any
+
+  def find_with_any(components) do
+    @client.find_with_any(components)
+  end
+
+  def find_with_any(components, keys) do
+    @client.find_with_any(components, keys)
+  end
+
+  def find_with_any(components, keys, functions) do
+    @client.find_with_any(components, keys, functions)
+  end
+
+  # read
+
+  def read(ids) when is_list(ids), do: @client.read(ids)
+  def read(id), do: hd(read([id]))
+
+  def read(ids, components) when is_list(ids) do
+    @client.read(ids, enforce_list(components))
+  end
+
+  def read(id, components), do: hd(read([id], components))
+
+  def read(ids, components, keys) when is_list(ids) do
+    @client.read(ids, enforce_list(components), enforce_list(keys))
+  end
+
+  def read(id, components, keys), do: hd(read([id], components, keys))
+
+  # write
+
+  def write(ids, components, keys, value) when is_list(ids) do
+    @client.write(ids, enforce_list(components), enforce_list(keys), value)
+  end
+
+  def write(id, components, keys, value) do
+    hd(write([id], components, keys, value))
+  end
+
+
+  #
+  # Private functions
+  #
+
+
+  defp enforce_list(value) when is_list(value), do: value
+  defp enforce_list(value), do: [value]
 end
--- test/exces_test.exs
+++ test/exces_test.exs
@@ -1,8 +1,399 @@
 defmodule ExecsTest do
+  import Execs
   use ExUnit.Case
   doctest Execs
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  setup_all [:setup_database]
+
+  describe "delete tests:" do
+    setup [:setup_context]
+
+    test "delete empty entity", %{id: id} = _context do
+      assert transaction(fn -> delete([id]) end) == [%{id: id, components: %{}}]
+      assert transaction(fn -> delete(id) end) == %{id: id, components: %{}}
+    end
+
+    test "delete nonempty entity", %{id: id} = _context do
+      assert transaction(fn -> write([id], [:foo], [:bar], :foobar) end) == [id]
+
+      assert transaction(fn ->
+        delete(id)
+      end) == %{id: id, components: %{foo: %{bar: :foobar}}}
+    end
+
+    test "delete single component", %{id: id} = _context do
+      transaction(fn ->
+        write([id], [:foo, :bar], [:foo, :bar], :foobar)
+      end)
+
+      assert transaction(fn ->
+        delete(id, [:bar])
+      end) == %{id: id, components: %{bar: %{foo: :foobar, bar: :foobar}}}
+
+      assert transaction(fn ->
+        read(id)
+      end) == %{id: id, components: %{foo: %{bar: :foobar, foo: :foobar}}}
+    end
+
+    test "delete single key", %{id: id} = _context do
+      transaction(fn ->
+        write([id], [:foo, :bar], [:foo, :bar], :foobar)
+      end)
+
+      assert transaction(fn ->
+        delete(id, [:bar], [:foo])
+      end) == %{id: id, components: %{bar: %{foo: :foobar}}}
+
+      assert transaction(fn ->
+        read([id])
+      end) == [
+        %{id: id,
+          components: %{foo: %{bar: :foobar, foo: :foobar},
+                        bar: %{bar: :foobar}}}
+      ]
+    end
+  end
+
+  describe "has* tests:" do
+      setup [:setup_context]
+
+      test "has_all", %{id: id} = _context do
+        id2 = new_entity()
+
+        transaction(fn ->
+          write([id, id2], [:foo, :bar], [:foo, :bar], :foobar)
+          write([id], [:foobar], [:foo], :foobar)
+        end)
+
+        assert transaction(fn ->
+          has_all(id, [:bar])
+        end) == %{id: id, result: true}
+
+        assert transaction(fn ->
+          has_all(id, [:bar, :foo])
+        end) == %{id: id, result: true}
+
+        assert transaction(fn ->
+          has_all([id, id2], [:bar, :foo])
+        end) == [%{id: id, result: true}, %{id: id2, result: true}]
+
+        assert transaction(fn ->
+          has_all([id, id2], [:bar, :foo, :foobar])
+        end) == [%{id: id, result: true}, %{id: id2, result: false}]
+
+        assert transaction(fn ->
+          has_all([id, id2], [:bar, :foo, :foobar], [:foo, :bar, :foobar])
+        end) == [%{id: id, result: false}, %{id: id2, result: false}]
+
+        assert transaction(fn ->
+          has_all([id, id2], [:bar, :foo], [:foo, :bar])
+        end) == [%{id: id, result: true}, %{id: id2, result: true}]
+
+        assert transaction(fn ->
+          has_all([id, id2],
+                  [:bar, :foo],
+                  [:foo, :bar],
+                  [&(&1 === :foobar), fn(_) -> true end])
+        end) == [%{id: id, result: true}, %{id: id2, result: true}]
+
+        assert transaction(fn ->
+          has_all([id, id2],
+                  [:bar, :foo],
+                  [:foo, :bar],
+                  [&(&1 === :foobar), fn(_) -> false end])
+        end) == [%{id: id, result: false}, %{id: id2, result: false}]
+      end
+
+      test "has_any", %{id: id} = _context do
+        id2 = new_entity()
+
+        transaction(fn ->
+          write([id], [:foo, :bar, :foobar], [:bar], :foobar)
+          write([id2], [:foo, :bar], [:bar], :foobar)
+        end)
+
+        assert transaction(fn ->
+          has_any(id, [:bar])
+        end) == %{id: id, result: true}
+
+        assert transaction(fn ->
+          has_any(id, [:bar, :baz])
+        end) == %{id: id, result: true}
+
+        assert transaction(fn ->
+          has_any(id, [:baz])
+        end) == %{id: id, result: false}
+
+        assert transaction(fn ->
+          has_any([id, id2], [:bar, :baz])
+        end) == [%{id: id, result: true}, %{id: id2, result: true}]
+
+        assert transaction(fn ->
+          has_any([id, id2], [:foobar])
+        end) == [%{id: id, result: true}, %{id: id2, result: false}]
+
+        assert transaction(fn ->
+          has_any([id, id2], [:foobar], [:bar])
+        end) == [%{id: id, result: true}, %{id: id2, result: false}]
+
+        assert transaction(fn ->
+          has_any([id, id2],
+                  [:foobar],
+                  [:bar],
+                  [&(&1 === :foobar), fn(_) -> true end])
+        end) == [%{id: id, result: true}, %{id: id2, result: false}]
+      end
+
+      test "has_which", %{id: id} = _context do
+        id2 = new_entity()
+
+        transaction(fn ->
+          write([id], [:foo, :bar, :foobar], [:bar], :foobar)
+          write([id2], [:foo, :bar], [:bar], :foobar)
+        end)
+
+        assert transaction(fn ->
+          has_which(id, [:bar])
+        end) == %{id: id, components: %{bar: true}}
+
+        assert transaction(fn ->
+          has_which(id, [:oof])
+        end) == %{id: id, components: %{oof: false}}
+
+        assert transaction(fn ->
+          has_which(id, [:bar, :oof])
+        end) == %{id: id, components: %{oof: false, bar: true}}
+
+        assert transaction(fn ->
+          has_which([id, id2], [:bar, :oof])
+        end) == [%{id: id, components: %{oof: false, bar: true}}, %{id: id2, components: %{oof: false, bar: true}}]
+
+        assert transaction(fn ->
+          has_which([id, id2], [:bar, :oof], [:bar, :foo])
+        end) == [
+          %{id: id,
+            components: %{bar: %{bar: true, foo: false},
+                          oof: %{bar: false, foo: false}}},
+          %{id: id2,
+            components: %{bar: %{bar: true, foo: false},
+                          oof: %{bar: false, foo: false}}}
+        ]
+
+        assert transaction(fn ->
+          has_which([id, id2],
+                    [:foo, :bar],
+                    [:bar, :foo],
+                    [&(&1 === :foobar), fn(_) -> true end])
+        end) == [
+          %{id: id,
+            components: %{bar: %{bar: true, foo: false},
+                          foo: %{bar: true, foo: false}}},
+          %{id: id2,
+            components: %{bar: %{bar: true, foo: false},
+                          foo: %{bar: true, foo: false}}}
+        ]
+      end
+    end
+
+  describe "list tests:" do
+    setup [:setup_context]
+
+    test "list", %{id: id} = _context do
+      id2 = new_entity()
+
+      transaction(fn ->
+        write([id, id2], [:foo, :bar], [:bar, :foo], :foobar)
+      end)
+
+      assert transaction(fn ->
+        list(id)
+      end) == %{id: id, components: [:bar, :foo]}
+
+      assert transaction(fn ->
+        list([id, id2], [:foo, :bar])
+      end) == [
+        %{id: id,
+          components: %{bar: [:bar, :foo],
+                        foo: [:bar, :foo]}},
+        %{id: id2,
+          components: %{bar: [:bar, :foo],
+                        foo: [:bar, :foo]}}
+      ]
+    end
+  end
+
+  describe "find* tests:" do
+    setup [:setup_context]
+
+    test "find_with_all", %{id: id} = _context do
+      id2 = new_entity()
+
+      transaction(fn ->
+        write([id, id2], [:oof, :rab], [:oof, :rab], :raboof)
+      end)
+
+      assert transaction(fn ->
+        find_with_all([:oof, :rab])
+      end) == [id, id2]
+
+      assert transaction(fn ->
+        find_with_all([:oof, :rabbar])
+      end) == []
+
+      assert transaction(fn ->
+        find_with_all([:oof, :rab], [:oof, :rab])
+      end) == [id, id2]
+
+      assert transaction(fn ->
+        find_with_all([:oof, :rab], [:oof, :rabbar])
+      end) == []
+
+      assert transaction(fn ->
+        find_with_all([:oof, :rab],
+                      [:oof, :rab],
+                      [&(&1 === :raboof), fn(_) -> true end])
+      end) == [id, id2]
+
+      assert transaction(fn ->
+        find_with_all([:oof, :rab],
+                      [:oof, :rab],
+                      [&(&1 === :foobar), fn(_) -> true end])
+      end) == []
+    end
+
+      test "find_with_any", %{id: id} = _context do
+        id2 = new_entity()
+
+        transaction(fn ->
+          write([id, id2], [:blarg, :blarb], [:blarg, :blarb], :meepmeep)
+        end)
+
+        assert transaction(fn ->
+          find_with_any([:blarg, :glarb])
+        end) == [id, id2]
+
+        assert transaction(fn ->
+          find_with_any([:glarb])
+        end) == []
+
+        assert transaction(fn ->
+          find_with_any([:blarg, :glarb], [:blarg, :glarb])
+        end) == [id, id2]
+
+        assert transaction(fn ->
+          find_with_any([:blarg, :glarb],
+                        [:blarg, :glarb],
+                        [&(&1 === :raboof), fn(_) -> true end])
+        end) == [id, id2]
+
+        assert transaction(fn ->
+          find_with_any([:blarg, :glarb],
+                        [:blarg, :glarb],
+                        [&(&1 === :foobar)])
+        end) == []
+      end
+    end
+
+  describe "read tests:" do
+    setup [:setup_context]
+
+    test "read", %{id: id} = _context do
+      id2 = new_entity()
+
+      assert transaction(fn ->
+        write([id, id2], [:foo, :bar], [:foo, :bar], :foobar)
+      end) == [id, id2]
+
+      assert transaction(fn ->
+        read([id, id2])
+      end) == [
+          %{id: id,
+           components: %{foo: %{bar: :foobar, foo: :foobar},
+                         bar: %{bar: :foobar, foo: :foobar}}},
+          %{id: id2,
+            components: %{foo: %{bar: :foobar, foo: :foobar},
+                          bar: %{bar: :foobar, foo: :foobar}}}
+        ]
+
+      assert transaction(fn ->
+        read([id, id2], [:foo, :bar])
+      end) == [
+        %{id: id,
+          components: %{foo: %{bar: :foobar, foo: :foobar},
+                        bar: %{bar: :foobar, foo: :foobar}}},
+        %{id: id2,
+          components: %{foo: %{bar: :foobar, foo: :foobar},
+                        bar: %{bar: :foobar, foo: :foobar}}}
+      ]
+
+      assert transaction(fn ->
+        read([id, id2], [:foo, :bar], [:foo, :bar])
+      end) ==[
+        %{id: id,
+          components: %{foo: %{bar: :foobar, foo: :foobar},
+                        bar: %{bar: :foobar, foo: :foobar}}},
+        %{id: id2,
+          components: %{foo: %{bar: :foobar, foo: :foobar},
+                        bar: %{bar: :foobar, foo: :foobar}}}
+      ]
+
+      assert transaction(fn ->
+        read(id, [:foo, :bar], [:foo])
+      end) ==
+        %{id: id,
+          components: %{foo: %{foo: :foobar},
+                        bar: %{foo: :foobar}}}
+
+      assert transaction(fn ->
+        read(id, [:foo], [:foo, :bar])
+      end) ==
+        %{id: id,
+          components: %{foo: %{bar: :foobar, foo: :foobar}}}
+    end
+  end
+
+  describe "write tests:" do
+    setup [:setup_context]
+
+    test "write", %{id: id} = _context do
+      id2 = new_entity()
+
+      assert transaction(fn ->
+        write(id, [:foo], [:bar], :foobar)
+      end) == id
+
+      assert transaction(fn ->
+        write([id, id2], [:foo], [:bar], :foobar)
+      end) == [id, id2]
+
+      assert transaction(fn ->
+        write([id, id2], [:foo, :bar], [:bar, :foo], :foobar)
+      end) == [id, id2]
+
+      assert transaction(fn ->
+        read([id, id2])
+      end) == [
+        %{id: id,
+          components: %{foo: %{bar: :foobar, foo: :foobar},
+                        bar: %{bar: :foobar, foo: :foobar}}},
+        %{id: id2,
+          components: %{foo: %{bar: :foobar, foo: :foobar},
+                        bar: %{bar: :foobar, foo: :foobar}}}
+      ]
+
+    end
+  end
+
+
+  defp setup_context(_context) do
+    %{id: new_entity()}
+  end
+
+  defp setup_database(_context) do
+    initialize()
+  end
+
+  defp new_entity do
+    create()
   end
 end

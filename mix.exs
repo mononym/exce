4dbb595cbceca84f66809922cea39e2d649889d0
Chris Hicks <khandrish@gmail.com>
2016-11-01 00:30:35 -0700

    prepare to publish mix package

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule Exces.Mixfile do
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps()]
+     deps: deps(),
+     description: description(),
+     package: package()]
   end
 
   def application do
@@ -16,5 +18,22 @@ defmodule Exces.Mixfile do
 
   defp deps do
     [{:qlc, "~> 1.0"}]
+  end
+
+  defp description do
+    """
+    Provides an interface for reading and writing data in an Entity Component System. Note that it is up to the
+    consuming application to implement the 'Systems' as this package simply makes working with data easier. Uses Mnesia
+    disc_copy tables.
+    """
+  end
+
+  defp package do
+    [
+     name: :execs,
+     files: ["lib", "priv", "mix.exs", "README*", "LICENSE*"],
+     maintainers: ["Chris Hicks"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/mononym/execs"}]
   end
 end

7e6f7266f1a8194e1268e910ed1061500cb9a5f5
Chris Hicks <khandrish@gmail.com>
2016-11-02 22:13:01 -0700

    bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Exces.Mixfile do
 
   def project do
     [app: :execs,
-     version: "0.2.0",
+     version: "0.2.1",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,

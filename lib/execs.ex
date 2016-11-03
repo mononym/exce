5d5f09ded336ebd72f77997f0214ed0a4db5d461
Chris Hicks <khandrish@gmail.com>
2016-11-02 22:40:14 -0700

    update execs specs

--- lib/execs.ex
+++ lib/execs.ex
@@ -27,8 +27,18 @@ defmodule Execs do
   @type key_list :: [key]
   @type list_keys :: %{id: id, components: %{component => [key]}}
   @type list_keys_list :: [list_keys]
-  @type list_components :: %{id: id, components: component_list}
+  @type list_components :: %{id: id, components: maybe_component_list}
   @type list_components_list :: [list_components]
+  @type maybe_component_list :: component | component_list
+  @type maybe_component_match_list :: component_match | component_match_list
+  @type maybe_entity_list :: entity | entity_list
+  @type maybe_fun_list :: fun | fun_list
+  @type maybe_id_list :: id | id_list
+  @type maybe_id_match_list :: id_match | id_match_list
+  @type maybe_key_list :: key | key_list
+  @type maybe_key_match_list :: key_match | key_match_list
+  @type maybe_list_components_list :: list_components | list_components_list
+  @type maybe_list_keys_list :: list_keys | list_keys_list
 
 
   #
@@ -85,19 +95,18 @@ defmodule Execs do
   @doc """
   Delete a set of entities and all their data.
   """
-  @spec delete(id_list) :: entity_list
+  @spec delete(maybe_id_list) :: maybe_entity_list
   def delete(ids) when is_list(ids), do: client().delete(ids)
   def delete(id), do: hd(delete([id]))
 
   @doc """
   Delete a set of components and all their data from a set of entities.
   """
-  @spec delete(id_list, component_list) :: entity_list
+  @spec delete(maybe_id_list, maybe_component_list) :: maybe_entity_list
   def delete(ids, components) when is_list(ids) do
     client().delete(ids, enforce_list(components))
   end
 
-  @spec delete(id, component_list) :: entity
   def delete(id, components) do
     hd(delete([id], components))
   end
@@ -105,12 +114,11 @@ defmodule Execs do
   @doc """
   Delete a set of keys from a set of components which belong to a set of entities.
   """
-  @spec delete(id_list, component_list, key_list) :: entity_list
+  @spec delete(maybe_id_list, maybe_component_list, maybe_key_list) :: maybe_entity_list
   def delete(ids, components, keys) when is_list(ids) do
     client().delete(ids, enforce_list(components), enforce_list(keys))
   end
 
-  @spec delete(id, component_list, key_list) :: entity
   def delete(id, components, keys) do
     hd(delete([id], components, keys))
   end
@@ -118,23 +126,21 @@ defmodule Execs do
   @doc """
   Check to see if a set of entities has a set of components.
   """
-  @spec has_all(id_list, component_list) :: id_match_list
+  @spec has_all(maybe_id_list, maybe_component_list) :: maybe_id_match_list
   def has_all(ids, components) when is_list(ids) do
     client().has_all(ids, enforce_list(components))
   end
 
-  @spec has_all(id, component_list) :: id_match
   def has_all(id, components), do: hd(has_all([id], components))
 
   @doc """
   Check to see if a set of entities has set of keys.
   """
-  @spec has_all(id_list, component_list, key_list) :: id_match_list
+  @spec has_all(maybe_id_list, maybe_component_list, maybe_key_list) :: maybe_id_match_list
   def has_all(ids, components, keys) when is_list(ids) do
     client().has_all(ids, enforce_list(components), enforce_list(keys))
   end
 
-  @spec has_all(id, component_list, key_list) :: id_match
   def has_all(id, components, keys), do: hd(has_all([id], components, keys))
 
   @doc """
@@ -142,12 +148,11 @@ defmodule Execs do
   is passed to each of the comparison functions. If all functions return true then there is
   a match.
   """
-  @spec has_all(id_list, component_list, key_list, fun_list) :: id_match_list
+  @spec has_all(maybe_id_list, maybe_component_list, maybe_key_list, maybe_fun_list) :: maybe_id_match_list
   def has_all(ids, components, keys, functions) when is_list(ids) do
     client().has_all(ids, enforce_list(components), enforce_list(keys), enforce_list(functions))
   end
 
-  @spec has_all(id, component_list, key_list) :: id_match
   def has_all(id, components, keys, functions) do
     hd(has_all([id], components, keys, functions))
   end
@@ -155,23 +160,21 @@ defmodule Execs do
   @doc """
   Check to see if a set of entities has at least one of a set of components.
   """
-  @spec has_any(id_list, component_list) :: id_match_list
+  @spec has_any(maybe_id_list, maybe_component_list) :: maybe_id_match_list
   def has_any(ids, components) when is_list(ids) do
     client().has_any(ids, enforce_list(components))
   end
 
-  @spec has_any(id, component_list) :: id_match
   def has_any(id, components), do: hd(has_any([id], components))
 
   @doc """
   Check to see if a set of entities has at least one of a set of keys.
   """
-  @spec has_any(id_list, component_list, key_list) :: id_match_list
+  @spec has_any(maybe_id_list, maybe_component_list, maybe_key_list) :: maybe_id_match_list
   def has_any(ids, components, keys) when is_list(ids) do
     client().has_any(ids, enforce_list(components), enforce_list(keys))
   end
 
-  @spec has_any(id, component_list, key_list) :: id_match
   def has_any(id, components, keys), do: hd(has_any([id], components, keys))
 
   @doc """
@@ -179,12 +182,11 @@ defmodule Execs do
   with the each key is passed to each of the comparison functions. If all functions return
   true then there is a match.
   """
-  @spec has_any(id_list, component_list, key_list, fun_list) :: id_match_list
+  @spec has_any(maybe_id_list, maybe_component_list, maybe_key_list, maybe_fun_list) :: maybe_id_match_list
   def has_any(ids, components, keys, functions) when is_list(ids) do
     client().has_any(ids, enforce_list(components), enforce_list(keys), enforce_list(functions))
   end
 
-  @spec has_any(id, component_list, key_list, fun_list) :: id_match
   def has_any(id, components, keys, functions) do
     hd(has_any([id], components, keys, functions))
   end
@@ -192,23 +194,21 @@ defmodule Execs do
   @doc """
   Check to see which of a set of components a set of entities has.
   """
-  @spec has_which(id_list, component_list) :: component_match_list
+  @spec has_which(maybe_id_list, maybe_component_list) :: maybe_component_match_list
   def has_which(ids, components) when is_list(ids) do
     client().has_which(ids, enforce_list(components))
   end
 
-  @spec has_which(id, component_list) :: component_match
   def has_which(id, components), do: hd(has_which([id], components))
 
   @doc """
   Check to see which of a set of keys a set of entities has.
   """
-  @spec has_which(id_list, component_list, key_list) :: component_match_list
+  @spec has_which(maybe_id_list, maybe_component_list, maybe_key_list) ::maybe_component_match_list
   def has_which(ids, components, keys) when is_list(ids) do
     client().has_which(ids, enforce_list(components), enforce_list(keys))
   end
 
-  @spec has_which(id, component_list, key_list) :: component_match
   def has_which(id, components, keys), do: hd(has_which([id], components, keys))
 
   @doc """
@@ -216,12 +216,11 @@ defmodule Execs do
   with the each key is passed to each of the comparison functions. If all functions return
   true then there is a match.
   """
-  @spec has_which(id_list, component_list, key_list, fun_list) :: component_match_list
+  @spec has_which(maybe_id_list, maybe_component_list, maybe_key_list, maybe_fun_list) :: maybe_component_match_list
   def has_which(ids, components, keys, functions) when is_list(ids) do
     client().has_which(ids, enforce_list(components), enforce_list(keys), enforce_list(functions))
   end
 
-  @spec has_which(id, component_list, key_list, fun_list) :: component_match
   def has_which(id, components, keys, functions) do
     hd(has_which([id], components, keys, functions))
   end
@@ -229,27 +228,25 @@ defmodule Execs do
   @doc """
   List the components of a set of entities.
   """
-  @spec list(id_list) :: list_components_list
+  @spec list(maybe_id_list) :: maybe_list_components_list
   def list(ids) when is_list(ids), do: client().list(ids)
 
-  @spec list(id) :: list_components
   def list(id), do: hd(list([id]))
 
   @doc """
   List the keys belonging to a set of components of a set of entities.
   """
-  @spec list(id_list, component_list) :: list_keys_list
+  @spec list(maybe_id_list, maybe_component_list) :: maybe_list_keys_list
   def list(ids, components) when is_list(ids) do
     client().list(ids, enforce_list(components))
   end
 
-  @spec list(id, component_list) :: list_keys
   def list(id, components), do: hd(list([id], components))
 
   @doc """
   List the entities which have a set of components.
   """
-  @spec find_with_all(component_list) :: id_list
+  @spec find_with_all(maybe_component_list) :: id_list
   def find_with_all(components) do
     client().find_with_all(enforce_list(components))
   end
@@ -257,7 +254,7 @@ defmodule Execs do
   @doc """
   List the entities which have a set of keys.
   """
-  @spec find_with_all(component_list, key_list) :: id_list
+  @spec find_with_all(maybe_component_list, maybe_key_list) :: id_list
   def find_with_all(components, keys) do
     client().find_with_all(enforce_list(components), enforce_list(keys))
   end
@@ -267,7 +264,7 @@ defmodule Execs do
   with the each key is passed to each of the comparison functions. If all functions return
   true then there is a match.
   """
-  @spec find_with_all(component_list, key_list, fun_list) :: id_list
+  @spec find_with_all(maybe_component_list, maybe_key_list, maybe_fun_list) :: id_list
   def find_with_all(components, keys, functions) do
     client().find_with_all(enforce_list(components),
                      enforce_list(keys),
@@ -277,7 +274,7 @@ defmodule Execs do
   @doc """
   List the entities which have at least one of a set of components.
   """
-  @spec find_with_any(component_list) :: id_list
+  @spec find_with_any(maybe_component_list) :: id_list
   def find_with_any(components) do
     client().find_with_any(enforce_list(components))
   end
@@ -285,7 +282,7 @@ defmodule Execs do
   @doc """
   List the entities which have at least one of a set of keys.
   """
-  @spec find_with_any(component_list, key_list) :: id_list
+  @spec find_with_any(maybe_component_list, maybe_key_list) :: id_list
   def find_with_any(components, keys) do
     client().find_with_any(enforce_list(components), enforce_list(keys))
   end
@@ -295,7 +292,7 @@ defmodule Execs do
   with the each key is passed to each of the comparison functions. If all functions return
   true then there is a match.
   """
-  @spec find_with_any(component_list, key_list, fun_list) :: id_list
+  @spec find_with_any(maybe_component_list, maybe_key_list, maybe_fun_list) :: id_list
   def find_with_any(components, keys, functions) do
     client().find_with_any(enforce_list(components), enforce_list(keys), enforce_list(functions))
   end
@@ -303,43 +300,39 @@ defmodule Execs do
   @doc """
   Read a set of entities.
   """
-  @spec read(id_list) :: entity_list
+  @spec read(maybe_id_list) :: maybe_entity_list
   def read(ids) when is_list(ids), do: client().read(ids)
 
-  @spec read(id) :: entity
   def read(id), do: hd(read([id]))
 
   @doc """
   Read a set of components belonging to a set of entities.
   """
-  @spec read(id_list, component_list) :: entity_list
+  @spec read(maybe_id_list, maybe_component_list) :: maybe_entity_list
   def read(ids, components) when is_list(ids) do
     client().read(ids, enforce_list(components))
   end
 
-  @spec read(id, component_list) :: entity
   def read(id, components), do: hd(read([id], components))
 
   @doc """
   Read a set of keys belonging to a set of entities.
   """
-  @spec read(id_list, component_list, key_list) :: entity_list
+  @spec read(maybe_id_list, maybe_component_list, maybe_key_list) :: maybe_entity_list
   def read(ids, components, keys) when is_list(ids) do
     client().read(ids, enforce_list(components), enforce_list(keys))
   end
 
-  @spec read(id, component_list, key_list) :: entity
   def read(id, components, keys), do: hd(read([id], components, keys))
 
   @doc """
   Write a value to any combination of keys, components, and entities.
   """
-  @spec write(id_list, component_list, key_list, any()) :: id_list
+  @spec write(maybe_id_list, maybe_component_list, maybe_key_list, any()) :: maybe_id_list
   def write(ids, components, keys, value) when is_list(ids) do
     client().write(ids, enforce_list(components), enforce_list(keys), value)
   end
 
-  @spec write(id, component_list, key_list, any()) :: id
   def write(id, components, keys, value) do
     hd(write([id], components, keys, value))
   end
